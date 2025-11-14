3ffd7efbe4f080437cc0f3ff7f036823ad963ba2
karoluciecha <165387089+karoluciecha@users.noreply.github.com>
2025-11-14 13:40:07 +0000

    Minor WeekSeven improvements.

--- YearThreeSemesterOne/weekSixB/src/main/java/com/example/demo/models/Customer.java
+++ YearThreeSemesterOne/weekSixB/src/main/java/com/example/demo/models/Customer.java
@@ -1,18 +1,8 @@
 package com.example.demo.models;
 
 public class Customer {
-	private int id;
 	private String name;
 	private String email;
-	private String password;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
@@ -29,13 +19,4 @@ public class Customer {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 }
